0c7d1283b33a80aad62d8122ca1a0b23a829c8a5
HuynhThinh <hkthinh034@gmail.com>
2019-09-20 14:48:05 +0700

    fix start game andorid + ios

--- Assets/00Uwin/Scripts/LoadSceneStart.cs
+++ Assets/00Uwin/Scripts/LoadSceneStart.cs
@@ -1,17 +1,73 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class LoadSceneStart : MonoBehaviour {
 
     [SerializeField]
-    private string strGetLink;
-
-    private void Start()
+    private string urlGetScene;
+    List<Dictionary<string, string>> startValue;
+    public PopStartGame popupStart;
+    void Start()
     {
-        StartCoroutine(VKCommon.DownloadTextFromURL("https://script.google.com/macros/s/AKfycbzrpif9MibanmQPVU-5Oh6fLgnRPP5-4NyXWuuK/exec", (string strConfig) =>
+        StartCoroutine(VKCommon.DownloadTextFromURL(urlGetScene, (value) =>
         {
-            Debug.Log(strConfig);
+            startValue = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(value);
+
+            string hash = startValue[0]["hash"];
+            int canStartGame = int.Parse(startValue[0]["canStartGame"]);
+
+            if(canStartGame == 1)
+            {
+#if UNITY_ANDROID
+                string url = startValue[0]["urlAndroid"];
+                StartCoroutine(DownloadScene(url, hash));
+#elif UNITY_IOS
+                string url = startValue[0]["urlIOS"];
+                StartCoroutine(DownloadScene(url, hash));
+#endif
+            }
+            else
+            {
+                string message = startValue[0]["message"];
+                popupStart.SetNotice(message);
+
+                return;
+            }
         }));
+    }
+
+    private IEnumerator DownloadScene(string urlSceneBundle, string hash)
+    {
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(urlSceneBundle, Hash128.Parse(hash), 0);
+        UnityWebRequestAsyncOperation operation = request.SendWebRequest();
+
+        ulong totalBytes = 0;
+        while (!operation.isDone)
+        {
+            if (totalBytes == 0)
+            {
+                string length = request.GetResponseHeader("Content-Length");
+                if (length != null)
+                {
+                    totalBytes = ulong.Parse(length);
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+        AssetBundle assetBundle = DownloadHandlerAssetBundle.GetContent(request);
+
+        if (assetBundle.isStreamedSceneAssetBundle)
+        {
+            string[] scenePaths = assetBundle.GetAllScenePaths();
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePaths[0]);
+
+            AsyncOperation async = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+            if (async == null)
+                yield break;
+        }
     }
 }
