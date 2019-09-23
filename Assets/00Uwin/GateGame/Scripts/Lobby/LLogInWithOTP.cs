7e5ea2485b5c788a999c19d40220432d74418699
HuynhThinh <hkthinh034@gmail.com>
2019-09-23 23:44:08 +0700

    up android

--- Assets/00Uwin/GateGame/Scripts/Lobby/LLogInWithOTP.cs
+++ Assets/00Uwin/GateGame/Scripts/Lobby/LLogInWithOTP.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LLogInWithOTP : UILayer
+public class  LLogInWithOTP : UILayer
 {
     [Space(40)]
     public Button btClose;
